d3276e7b6d073d2a755a8054816831af2c6fed07
bljboy <bljboy55@gmail.com>
2020-06-15 19:55:54 +0800

    Toast_blj

--- app/src/main/java/com/example/demo0615/MainActivity.java
+++ app/src/main/java/com/example/demo0615/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.demo0615;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "我是blj！", Toast.LENGTH_LONG).show();
     }
 }
